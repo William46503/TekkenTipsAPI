82ca2f098830b243932e93ec8369655d6ee6b523
William46503 <52475057+William46503@users.noreply.github.com>
2020-06-29 22:24:58 +0900

    Added: Create a document if Charname doesn't exist, else push item to "Weakness" Array, Through postman

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/TekkenTDB", {
@@ -17,16 +19,45 @@ mongoose.connect("mongodb://localhost:27017/TekkenTDB", {
 });
 
 const tekken7Schema = ({
-  CharName : String,
-  Weakness : [String]
+  CharName: String,
+  Weakness: [String]
 });
 
 const Document = new mongoose.model("document", tekken7Schema);
 
-app.get("/", function(req, res){
-  console.log("Website is up");
+
+app.post("/CharacterItem", function(req, res) {
+  const reqCharName = req.body.character;
+  const reqCharContent = req.body.characterContent;
+
+  Document.findOne({
+    CharName: reqCharName
+  }, function(err, foundDoc) {
+    if (!err) {
+      if (!foundDoc) {
+        const newDoc = new Document({
+          CharName: reqCharName,
+          Weakness: reqCharContent
+        });
+        newDoc.save();
+      } else {
+        foundDoc.Weakness.push(reqCharContent);
+        foundDoc.save();
+      }
+    };
+  });
 });
 
-app.listen(3000, function(){
+
+
+app.get("/", function(req, res) {
+  Document.find({}, function(err, foundList) {
+    if (!err) {
+      res.send(foundList);
+    }
+  });
+});
+
+app.listen(3000, function() {
   console.log("Server is up at port 3000");
 });
